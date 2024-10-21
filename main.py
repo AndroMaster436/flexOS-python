051ccf77f58f90c7203706d535d4c8d8288df087
Andro_Dev <87223939+AndroMaster436@users.noreply.github.com>
2024-10-21 18:12:47 +0300

    add boot animation

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from Core import Kernel
+import curses, time
 
-if __name__ == "__main__":
-    # todo
-    exit()
+def init(stdscr):
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    while True:
+        stdscr.clear()
+        stdscr.bkgd(' ', curses.color_pair(1))
+
+        title = "Запуск flexOS..."
+        frames = ["[                    ]", "[=                   ]", "[==                  ]", "[===                 ]", "[====                ]", "[=====               ]", "[======              ]", "[=======             ]", "[========            ]", "[=========           ]", "[==========          ]", "[===========         ]", "[============        ]", "[=============       ]", "[==============      ]", "[===============     ]", "[================    ]", "[=================   ]", "[==================  ]", "[=================== ]", "[====================]"]
+
+        height, width = stdscr.getmaxyx()
+        x_title = (width // 2) - (len(title) // 2)
+        stdscr.addstr(height // 2 - 2, x_title, title, curses.color_pair(1))
+
+        for frame in frames:
+            x_frame = (width // 2) - (len(frame) // 2)
+            stdscr.addstr(height // 2, x_frame, frame, curses.color_pair(1))
+            stdscr.refresh()
+            time.sleep(0.2)
+        break
+
+curses.wrapper(init)
